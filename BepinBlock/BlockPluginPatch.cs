fdb96778095deab9ccfc4a824e47d5560e9b3b1c
TeamDoodz <unconfigured@null.spigotmc.org>
2023-01-08 22:52:36 -0600

    Add project files.

---
+++ BepinBlock/BlockPluginPatch.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using BepInEx;
+using BepInEx.Bootstrap;
+using BepInEx.Configuration;
+using HarmonyLib;
+using UnityEngine;
+using Mono.Cecil;
+using System.Reflection;
+
+namespace BepinBlock;
+
+// Whenever GameObject.AddComponent(Type) is called, look at the parameter and object.
+// If the gameobject in question is Chainloader.ManagerObject and the parameter is a type assignable to BaseUnityPlugin (and isn't our plugin, although that shouldn't matter)
+// throw an exception if the type in question is blocked by the config. (If an exception is thrown when creating a plugin component, that plugin will be unloaded)
+
+internal static class BlockPluginPatch {
+	static ConfigFile cfg = new ConfigFile(Path.Combine(Paths.ConfigPath, "block-plugins.cfg"), false);
+	static string HarmonyID => MainPlugin.GUID + ".blocker";
+	static Harmony harmony = new(HarmonyID);
+
+	public static void Initialize() {
+		harmony.PatchAll(typeof(BlockPluginPatch));
+	}
+
+	static MethodBase TargetMethod() {
+		return typeof(GameObject).GetMethod("AddComponent", new Type[] { typeof(Type) });
+	}
+
+	static void Prefix(GameObject __instance, Type componentType) {
+		if(__instance != Chainloader.ManagerObject) {
+			return;
+		}
+		if(componentType.GetCustomAttribute<BepInPlugin>() is not BepInPlugin attribute) {
+			return;
+		}
+		if(!GetModCfg(attribute)) {
+			return;
+		}
+
+		throw new Exception("This plugin has been disabled.");
+	}
+
+	static bool GetModCfg(BepInPlugin plugin) {
+		return cfg.Bind(new ConfigDefinition("plugins", plugin.GUID), true, new ConfigDescription(plugin.Name)).Value;
+	}
+}
---
+++ BepinBlock/MainPlugin.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using BepInEx;
+using BepInEx.Bootstrap;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+
+namespace BepinBlock;
+
+[BepInPlugin(GUID, NAME, VERSION)]
+public sealed class MainPlugin : BaseUnityPlugin {
+
+	public const string GUID = "00io.github.TeamDoodz.BepinBlock"; // Place zeros at start of GUID so that this mod is loaded first
+	public const string NAME = "BepinBlock";
+	public const string VERSION = "1.0.0";
+
+	internal static ManualLogSource? logger;
+
+	// Awake is called the moment the plugin component is created.
+	private void Awake() {
+		Logger.LogMessage("BepinBlock was loaded.");
+		logger = Logger;
+		BlockPluginPatch.Initialize();
+	}
+
+	// Start is called on the first update frame, i.e. some time after all plugins are loaded.
+	private void Start() {
+		Logger.LogMessage("All plugins finished loading.");
+	}
+}
