cc5ee5387a4f4d91c29e146bd7317d20340e57be
TeamDoodz <unconfigured@null.spigotmc.org>
2023-01-23 13:54:37 -0600

    add attribute and test plugin

---
+++ BepinBlock.UnloadExample/UnloadExamplePlugin.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using BepInEx;
+using BepInEx.Bootstrap;
+using UnityEngine;
+using BepinBlock.RuntimeUnloading;
+
+namespace BepinBlock.UnloadExample;
+
+[BepInPlugin(GUID, NAME, VERSION), BepInDependency("00io.github.TeamDoodz.BepinBlock"), SupportsRuntimeUnloading]
+public sealed class UnloadExamplePlugin : BaseUnityPlugin {
+
+	public const string GUID = "io.github.TeamDoodz.BepinBlock.UnloadExample";
+	public const string NAME = "BepinBlock.UnloadExample";
+	public const string VERSION = "1.0.0";
+
+	bool isQuitting = false;
+
+	private void Awake() {
+		Application.quitting += () => isQuitting = true;
+		Logger.LogMessage("Mod being loaded");
+	}
+
+	private void OnDestroy() {
+		if(isQuitting) {
+			return;
+		}
+		Logger.LogMessage("Mod being unloaded");
+	}
+}
---
+++ BepinBlock/RuntimeUnloading/SupportsRuntimeUnloadingAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BepinBlock.RuntimeUnloading;
+
+[AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
+public sealed class SupportsRuntimeUnloadingAttribute : Attribute {
+    public SupportsRuntimeUnloadingAttribute() {
+    }
+}
